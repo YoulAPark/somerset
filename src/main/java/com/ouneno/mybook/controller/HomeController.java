a74843215aa43fc4c8b05b09027e25586e282d93
ouneno <dbfdk2531@gmail.com>
2022-08-24 01:06:22 +0900

    2022-08-24 01:04 1. 개별메모수정 2. mybookDao 추가 	- 도메인 추가 	- @Getter, @Setter, @ToString annotation 지정완료 3. HomeController 추가 	- RequestMapping("/") 지정으로 메인페이지 기본메서드 지정 	- 연습용으로 get()메서드 추가

--- src/main/java/com/ouneno/mybook/controller/HomeController.java
+++ src/main/java/com/ouneno/mybook/controller/HomeController.java
@@ -1,28 +1,33 @@
 package com.ouneno.mybook.controller;
 
 import javax.servlet.http.HttpServletRequest;
-
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 
-	@Controller
-	public class HomeController {
-		@RequestMapping(value = "/home", method=RequestMethod.GET)
-		@ResponseBody
-		public String goHome(HttpServletRequest request) {
-			System.out.println("enter");
-			return "Hello Spring Boot";
-		}
-		
-	@Controller
-	public class StartController {
-		@RequestMapping(value = "/test", method=RequestMethod.GET)
-		@ResponseBody
-		public String goTest(HttpServletRequest request) {
-			System.out.println("StartController 시작");
-			return "Start Controller page 입니다.";
-		}
+@Controller
+// HomeController : 가장 기본 메인페이지 실행
+public class HomeController {
+	
+	// 메인페이지 실행
+	@RequestMapping("/")
+	public String main(Model model) {
+		System.out.println("/main"); // localhost:8080 실행시 STS 콘솔창에 확인 됨
+		return "main";
 	}
+	
+	// 연습용(GET방식)
+	/*
+	 *  localhost:8080/get 실행시 STS콘솔창에 "enter"가 출력되며,
+	 *  웹페이지 상으로 "Hello Spring Boot"가 출력된다.
+	 */
+	@RequestMapping(value = "/get", method=RequestMethod.GET)
+	@ResponseBody
+	public String get(HttpServletRequest request) {
+		System.out.println("STS 콘솔출력 : HomeController의 get()메서드 실행");
+		return "웹페이지 출력 : HomeController의 get()메서드 실행";
+	}
+	
 }
--- src/main/java/com/ouneno/mybook/service/domain/mybookDao.java
+++ src/main/java/com/ouneno/mybook/service/domain/mybookDao.java
@@ -1,5 +1,16 @@
 package com.ouneno.mybook.service.domain;
 
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter
+@Setter
+@ToString
 public class mybookDao {
+	
+	private int id;
+	private String name;
+	private String email;
 
 }
